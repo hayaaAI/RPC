33700f3033f93cf9c58cdc9aacecc1df29f0e3d0
hayaa <dev@hayaa-ai.com>
2018-07-20 01:35:26 +0800

    调整项目结构

---
+++ C#/RPC/Hayaa.RPC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Hayaa.RPC
+{
+    public class Class1
+    {
+    }
+}
---
+++ C#/RPC/Hayaa.RPC/Config/ConfigHelper.cs
@@ -0,0 +1,18 @@
+﻿using Hayaa.ConfigSeed.Standard;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.RPC.Common.Config
+{
+    internal class ConfigHelper : ConfigTool<RPCConfig, RPCRootConfig>
+    {
+        private static ConfigHelper _instance = new ConfigHelper();
+        private ConfigHelper() : base(DefineTable.RPCComponentID)
+        {
+
+        }
+
+        internal static ConfigHelper Instance { get => _instance; }
+    }
+}
