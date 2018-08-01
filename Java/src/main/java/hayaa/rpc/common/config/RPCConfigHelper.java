ab66450dada4ea768fd844a1cf76ef9b88727773
hayaa <dev@hayaa-ai.com>
2018-08-01 11:49:29 +0800

    调整项目结构

---
+++ Java/src/main/java/hayaa/rpc/common/config/RPCConfigHelper.java
@@ -0,0 +1,25 @@
+package hayaa.rpc.common.config;
+
+public class RPCConfigHelper {
+    public static RPCConfig.ProviderConfig getProviderConfig(){
+        RPCConfig rpcConfig= ConfigHelper.getInstance().GetComponentConfig();
+        if((rpcConfig!=null)&&(rpcConfig.getProviderConfiguation()!=null)){
+            return  rpcConfig.getProviderConfiguation();
+        }
+        return null;
+    }
+    public static RPCConfig.ConsumerConfig getConsumerConfig(){
+        RPCConfig rpcConfig= ConfigHelper.getInstance().GetComponentConfig();
+        if((rpcConfig!=null)&&(rpcConfig.getConsumerConfiguation()!=null)){
+            return  rpcConfig.getConsumerConfiguation();
+        }
+        return null;
+    }
+    public static RPCConfig getRPCConfig(){
+        RPCConfig rpcConfig= ConfigHelper.getInstance().GetComponentConfig();
+        if(rpcConfig!=null){
+            return  rpcConfig;
+        }
+        return null;
+    }
+}
